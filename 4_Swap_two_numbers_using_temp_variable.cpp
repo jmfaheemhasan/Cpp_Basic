7f9cb2879ddfff0f0f93f73995c448b93b9c6edc
jmfaheemhasan <jmfahimhasan52@gmail.com>
2025-10-31 00:30:49 +0600

    4_Swap_two_numbers_using_temp_variable.cpp

---
+++ 4_Swap_two_numbers_using_temp_variable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int a=300,b=700;
+
+cout<<"Before Swap:a="<<a<<",b="<<b<<endl;
+
+int temp=a;
+a=b;
+b=temp;
+
+cout<<"after swap:a="<<a<<",b="<<b<<endl;
+
+return 0;
+}
+
